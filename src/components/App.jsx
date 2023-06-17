f8e4af381c477167960aa7819ef663128177ab41
kristinaordenans <okristin92@gmail.com>
2023-06-17 15:24:06 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <input type="" name="" id="" />
+      <button>Add contact</button>
     </div>
   );
 };
